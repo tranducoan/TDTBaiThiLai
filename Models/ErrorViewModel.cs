ecc8100012de12c10daa5ccfb0fe5aa1327e4845
tranductoan <100502387+tranducoan@users.noreply.github.com>
2024-06-21 15:19:12 +0700

    TDT642 câu 1

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -5,4 +5,12 @@ public class ErrorViewModel
     public string? RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    //Sử dụng code first (xây dựng class trước, từ class sinh ra CSDL) để ánh xạ các class vào CSDL
+//Chạy lệnh dotnet ef migrations add Create_table_Person
+
+//Chạy lệnh dotnet ef database update để cập nhật cơ sở dữ liệu
+//* sinh mã tự động 
+//dotnet aspnet-codegenerator controller -name EmployeeController -m Employee -dc MvcMovie.Data.ApplicationDbContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries --databaseProvider sqlite
+// 
 }
---
+++ Models/TDT642.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TDTBaiThiLai.Models
+{
+    [Table("")]
+    public class TDT642
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Day { get; set; }
+// TDT642 
+    }
+}
